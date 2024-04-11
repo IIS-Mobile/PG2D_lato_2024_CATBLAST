025ebb68965988e5284be8f6cb510b1ac70ff637
ReQss <ReQsPL@gmail.com>
2024-04-11 21:16:19 +0200

    Dash commit

--- Scripts/Player/Dash.gd
+++ Scripts/Player/Dash.gd
@@ -7,6 +7,6 @@ func set_property(tx_pos, tx_scale):
 	scale = tx_scale
 func ghosting():
 	var tween_fade = get_tree().create_tween()
-	tween_fade.tween_property(self, "self_modulate",Color(1, 1, 1, 0),0.75)
+	tween_fade.tween_property(self, "self_modulate",Color(1, 1, 1, 0),0.45)
 	await tween_fade.finished
 	queue_free()
--- Scripts/Player/player_movement.gd
+++ Scripts/Player/player_movement.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+const DASH_SPEED = 1500
+const DASH_UP = -600
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
@@ -9,23 +10,21 @@ const JUMP_VELOCITY = -500.0
 @onready var footsteps: AudioStream = load("res://Assets/Sounds/player/footstep - Pixabay.wav")
 @onready var jump: AudioStream = load("res://Assets/Sounds/player/jump - Soundsnap and Friends.wav")
 @onready var anim = get_node("AnimationPlayer")
-
+@onready var particles = $GPUParticles2D
 
 @export var ghost_node : PackedScene
 @onready var ghost_timer = $GhostTimer
-
+var dashing = false;
+var can_dash = true
 func add_ghost():
 	var ghost = ghost_node.instantiate()
-	ghost.set_property(position,$AnimatedSprite2D.scale)
+	ghost.set_property(global_position,$AnimatedSprite2D.scale)
 	get_tree().current_scene.add_child(ghost)
 
-	
+
 func dash():
-	ghost_timer.start()
-	var tween = get_tree().create_tween()
-	tween.tween_property(self,"position",position+velocity * 1.5,0.45)
-	await tween.finished
-	ghost_timer.stop()
+	particles.emitting = true
+	$GhostSpawnTimer.start()
 	
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -35,15 +34,28 @@ func _ready():
 	audio_player = $AudioStreamPlayer2D
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("Dash"):
-		dash()
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_just_pressed("Dash") and can_dash:
+		if direction:
+			dash()
+			dashing = true
+			can_dash = false
+			ghost_timer.start()
+			$dash_again_timer.start()
+		elif  Input.is_action_pressed("Jump"):
+			print("X")
+			dash()
+			dashing = true
+			can_dash = false
+			ghost_timer.start()
+			$dash_again_timer.start()
+			velocity.y = DASH_UP
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor() :
-		
 		velocity.y = JUMP_VELOCITY
 		if anim.current_animation != "Attack"and anim.current_animation != "attack_left":
 			anim.play("Jump")
@@ -53,7 +65,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	var dir = get_node("AnimatedSprite2D").flip_h
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
@@ -66,18 +78,20 @@ func _physics_process(delta):
 		else:
 			anim.play("attack_left")
 	if direction:
-		velocity.x = direction * SPEED
-		if( velocity.y == 0) and anim.current_animation != "Attack"  and anim.current_animation != "attack_left":
-			anim.play("Run")
-			if $Timer.time_left <= 0:
-				playsound(footsteps)
-				$Timer.start(0.34)
-		
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
+			if( velocity.y == 0) and anim.current_animation != "Attack"  and anim.current_animation != "attack_left":
+				anim.play("Run")
+				if $Timer.time_left <= 0:
+					playsound(footsteps)
+					$Timer.start(0.34)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if( velocity.y== 0) and anim.current_animation != "Attack"and anim.current_animation != "attack_left":
+		if( velocity.y== 0) and anim.current_animation != "Attack" and anim.current_animation != "attack_left":
 			anim.play("Idle")
-	if(velocity.y > 0) and anim.current_animation != "Attack"and anim.current_animation != "attack_left":
+	if(velocity.y > 0) and anim.current_animation != "Attack" and anim.current_animation != "attack_left":
 		anim.play("Fall")
 	move_and_slide()
 
@@ -87,7 +101,7 @@ func _on_weapon_area_2d_body_entered(body):
 		print("Hit enemy")
 		playsound(katanaSlashMetal)
 		body.queue_free()
-	pass # Replace with function body.
+	pass 
 
 func playsound(sound):
 	var random_index = randi() % 3
@@ -98,4 +112,13 @@ func playsound(sound):
 
 
 func _on_ghost_timer_timeout():
-	add_ghost()
+	dashing = false
+	particles.emitting = false
+
+func _on_dash_again_timer_timeout():
+	can_dash = true
+
+
+func _on_ghost_spawn_timer_timeout():
+	if dashing:
+		add_ghost()
