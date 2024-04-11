ade6105fc2c202f3f06dd677695caec2d3519a02
Artur Trębski <trebski.artur@gmail.com>
2024-04-11 13:30:29 +0200

    Adding better control options

--- Scripts/Player/player_movement.gd
+++ Scripts/Player/player_movement.gd
@@ -25,7 +25,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor() :
+	if Input.is_action_just_pressed("Jump") and is_on_floor() :
 		velocity.y = JUMP_VELOCITY
 		if anim.current_animation != "Attack"and anim.current_animation != "attack_left":
 			anim.play("Jump")
