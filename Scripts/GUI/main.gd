e7840a6f2ee1d174bc5126b8ad94eb94eaa95e97
ReQss <ReQsPL@gmail.com>
2024-04-17 23:31:27 +0200

    pause_ui fixed

--- Scripts/GUI/main.gd
+++ Scripts/GUI/main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+class_name GameManager
+
 @onready var menuMusic: AudioStream = load("res://Assets/Sounds/music/main menu theme - GreenStarFire.ogg")
 @onready var confirmSound: AudioStream = load("res://Assets/Sounds/ui/button yes - phoenix_the_maker.wav")
 @onready var hoverSound: AudioStream = load("res://Assets/Sounds/ui/hover button - Pixabay.wav")
@@ -7,41 +9,41 @@ extends Node2D
 
 var audio_player: AudioStreamPlayer2D
 var music_player: AudioStreamPlayer
-var paused = false;
+
+signal toggle_game_paused(is_paused: bool)
+var game_paused : bool = false :
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		get_tree().paused = game_paused
+		emit_signal("toggle_game_paused",game_paused)
+	
+func _input(event : InputEvent):
+	if(event.is_action_pressed("ui_cancel")):
+		game_paused = !game_paused
 
 func _ready():
 	audio_player = $AudioStreamPlayer2D
 	music_player = $AudioStreamPlayer
 	music_player.stream = menuMusic
 	music_player.play()
-	$Panel/Control/Play.grab_focus()
-func _on_quit_pressed():
-	get_tree().quit()
-
-
-func _on_play_pressed():
-	$AudioStreamPlayer.stop()
-	audio_player.stream = confirmSound
-	audio_player.play()
-	
-	get_tree().change_scene_to_file("res://Scenes/TestLevel.tscn")
-	
-	
-func _process(delta):
-	if Input.is_action_just_pressed("ui_end"):
-		get_tree().change_scene_to_file("res://Scenes/TestLevel.tscn")
-	if Input.is_action_just_pressed("ui_cancel"):
-		
-		pauseMenu()
-func pauseMenu():
-	print("esc")
-	if paused:
-		pause_menu.hide()
-		Engine.time_scale = 1
-	else:
-		pause_menu.show()
-		Engine.time_scale = 0
-	paused = !paused
+	#$CanvasLayer/PauseMenu/Play.grab_focus()
+#
+#func _on_play_pressed():
+	#$AudioStreamPlayer.stop()
+	#audio_player.stream = confirmSound
+	#audio_player.play()
+	#
+	#get_tree().change_scene_to_file("res://Scenes/TestLevel.tscn")
+#
+#func pauseMenu():
+	#var current_value : bool = get_tree().paused
+	#get_tree().paused = !current_value
+	#if get_tree().paused:
+		#pause_menu.hide()
+	#else:
+		#pause_menu.show()
 
 func _on_hover():
 	audio_player.stream = hoverSound
