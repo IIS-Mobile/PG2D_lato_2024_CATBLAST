01ccab621692cf3b2c9d5457b2e59ffa4578a8d0
Artur Trębski <trebski.artur@gmail.com>
2024-04-30 00:19:16 +0200

    Player no longer needs to run away from the NPC to be able to talk again

--- Scripts/NPC/yes_no_choice_panel.gd
+++ Scripts/NPC/yes_no_choice_panel.gd
@@ -18,9 +18,11 @@ func _on_yes_button_pressed():
 	print("IT IS AN OPPORTUNITY TO SWTICH BETWEEN LEVELS BY TALKING TO NPC")
 	choice_box.hide()
 	GlobalVariables.PLAYER_CONTROLS_ENABLED = true
+	GlobalVariables.IS_PLAYER_TALKING = false
 
 
 func _on_no_button_pressed():
 	print("NO...")
 	choice_box.hide()
 	GlobalVariables.PLAYER_CONTROLS_ENABLED = true
+	GlobalVariables.IS_PLAYER_TALKING = false
