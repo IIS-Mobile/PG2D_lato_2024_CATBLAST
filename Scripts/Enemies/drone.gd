d1eef9fdaefd08e9f4ec811fcb7553248e2fc898
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-05-09 14:36:32 +0200

    Add enemy drone

--- Scripts/Enemies/drone.gd
+++ Scripts/Enemies/drone.gd
@@ -1,11 +1,29 @@
-extends Node
+extends CharacterBody2D
 
+const SPEED = 60.0
 
-# Called when the node enters the scene tree for the first time.
+var player = null
+
+var bullet = preload("res://Scenes/Enemies/bullet.tscn")
+
 func _ready():
-	pass # Replace with function body.
+	player = get_node("/root/TestLevel/Player")
+
+func _physics_process(delta):
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		get_node("AnimatedSprite2D").flip_h = direction.x > 0
+		velocity.x = direction.x * SPEED
+		# velocity.y = direction.y * SPEED
+	
+	if randf() < 0.01:
+		spawn_bullet()
+
+	move_and_slide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func spawn_bullet():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.global_position = global_position
+	get_parent().add_child(bullet_instance)
+	get_node("AnimationPlayer").play("shoot")
