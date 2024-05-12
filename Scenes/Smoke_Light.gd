8e480f257e1a396fa1a26bd9798ec279fa2a5021
Jan Kozłowski <jasiokzl@gmail.com>
2024-05-12 16:30:49 +0200

    NPC graphics
    Sprite sheet, PointLight2D, Smoke Particles

---
+++ Scenes/Smoke_Light.gd
@@ -0,0 +1,15 @@
+extends PointLight2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#var current_frame: int = $AnimatedSprite2D.get_frame()
+	#if(current_frame >= 3 && current_frame <=8):
+		#set_position(Vector2(7, -6)) 
+	pass
