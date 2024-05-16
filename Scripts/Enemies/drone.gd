efd70152aff715956179181ec4d923208961e3cd
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-05-16 14:16:25 +0200

    Update drone animations

--- Scripts/Enemies/drone.gd
+++ Scripts/Enemies/drone.gd
@@ -2,11 +2,32 @@ extends CharacterBody2D
 
 const SPEED = 60.0
 
+const RANGE = 250.0
+
+const COOLDOWN = 3.0 # seconds
+
 @onready var player = get_node("/root/GameManager/Player")
 
-var bullet = preload("res://Scenes/Enemies/bullet.tscn")
+@onready var bullet = preload("res://Scenes/Enemies/bullet.tscn")
 
 @onready var animation_tree : AnimationTree = $AnimationTree
+
+@onready var anim_state_machine = $AnimationTree.get("parameters/playback")
+
+var timer = Timer.new()
+
+var timer2 = Timer.new()
+
+func _ready():
+	timer = Timer.new()
+	timer.set_wait_time(COOLDOWN)
+	timer.set_one_shot(true)
+	add_child(timer)
+	timer2 = Timer.new()
+	timer2.set_wait_time(0.1)
+	timer2.set_one_shot(true)
+	add_child(timer2)
+	
 
 func _physics_process(delta):
 	var direction = (player.global_position - global_position).normalized()
@@ -14,8 +35,21 @@ func _physics_process(delta):
 	velocity.x = direction.x * SPEED
 	# velocity.y = direction.y * SPEED
 	
-	if randf() < 0.01:
-		spawn_bullet()
+	# get current animation
+	var current_animation = anim_state_machine.get_current_node()
+	if current_animation == "shoot":
+		velocity.x = 0
+		velocity.y = 0
+
+	# if player is in range
+	if player.global_position.distance_to(global_position) < RANGE:
+		# if timer is counting
+		if timer.is_stopped():
+			animation_tree["parameters/conditions/shooting"] = true	
+			spawn_bullet()
+			timer.start()
+		else:
+			animation_tree["parameters/conditions/shooting"] = false
 
 	move_and_slide()
 
@@ -24,4 +58,5 @@ func spawn_bullet():
 	var bullet_instance = bullet.instantiate()
 	bullet_instance.global_position = global_position
 	get_parent().add_child(bullet_instance)
-	get_node("AnimatedSprite2D/AnimationPlayer").play("shoot")
+	# get_node("AnimatedSprite2D/AnimationPlayer").play("shoot")
+	# animation tree set shooting parameter to change anim
