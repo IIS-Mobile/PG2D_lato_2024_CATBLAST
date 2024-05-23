92f333652994cf9ea08c8be8b6511ec9004b6350
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-05-23 15:50:09 +0200

    Add laser to drone

---
+++ Scripts/Enemies/laser.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var player = null
+var direction = null
+
+@onready var timer = $Timer
+
+func _ready():
+	player = get_node("/root/GameManager/Player")
+	direction = (player.global_position - global_position).normalized()
+	rotation = direction.angle()
+	scale.x = 0
+
+	timer = Timer.new()
+	timer.wait_time = 10.0
+	timer.one_shot = true
+	var callable = Callable(self, "_on_Timer_timeout")
+	timer.connect("timeout", callable)
+	add_child(timer)
+	timer.start()
+
+func _physics_process(delta):
+	velocity.x = direction.x * SPEED
+	velocity.y = direction.y * SPEED
+	scale.x += 0.1
+	move_and_slide()
+
+func _on_Timer_timeout():
+	queue_free()
