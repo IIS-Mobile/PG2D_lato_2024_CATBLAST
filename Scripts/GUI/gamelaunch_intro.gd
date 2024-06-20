6883e6b1acd2f99aaa524b4d56c3da66e11c842f
Artur Trębski <trebski.artur@gmail.com>
2024-06-20 10:57:28 +0200

    Intro can be skipped

--- Scripts/GUI/gamelaunch_intro.gd
+++ Scripts/GUI/gamelaunch_intro.gd
@@ -1,10 +1,29 @@
 extends Node2D
 
+var skip_timers = false
 
 func _ready():
 	SoundtrackPlayer.play_soundtrack(SOUNDTRACKPLAYER_CLASS.THEMES.INTRO)
 	$AnimationPlayer.play("Fade_in")
-	await get_tree().create_timer(6).timeout
+	await _wait_or_skip(6)
+	if skip_timers:
+		return
 	$AnimationPlayer.play("Fade_out")
-	await get_tree().create_timer(3).timeout
+	await _wait_or_skip(3)
+	if skip_timers:
+		return
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
+
+
+func _wait_or_skip(seconds):
+	var timer = get_tree().create_timer(seconds)
+	while not skip_timers and not timer.time_left == 0:
+		await get_tree().process_frame
+	if skip_timers:
+		return
+
+
+func _input(event):
+	if event is InputEventMouseButton or event is InputEventKey:
+		skip_timers = true
+		get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
