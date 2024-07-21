dbb96dd31e55a7feb909c715933ac81af732a765
Jan Kozłowski <jasiokzl@gmail.com>
2024-07-21 23:22:55 +0200

    Implant_chest shader and animation

--- Scripts/Global/implant_chest.gd
+++ Scripts/Global/implant_chest.gd
@@ -5,6 +5,8 @@ extends StaticBody2D
 @onready var is_player_in_grab_range = false
 @onready var message_canvas_layer = $CanvasLayer
 @onready var message_timer = $MessageTimer
+@onready var animation_player = $CanvasLayer/AnimationPlayer
+var waiting_for_input = false
 
 func set_my_parameter(value):
 	my_parameter = value
@@ -12,6 +14,7 @@ func set_my_parameter(value):
 func _ready():
 	print("Aktualna wartość my_parameter: ", my_parameter)
 	message_timer.connect("timeout", Callable(self, "_on_message_timer_timeout"))
+	animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
 
 func _process(delta):
 	if !is_implant_chest_exhausted and is_player_in_grab_range and Input.is_action_just_pressed("Interact"):
@@ -34,7 +37,24 @@ func show_message(text):
 	var message_label = message_canvas_layer.get_node("MessageLabel")
 	message_label.text = text
 	$CanvasLayer/ImplantSprite.texture = load(GlobalVariables.IMPLANTS[my_parameter].graphic_path)
+	
+	# Play the animation to scale up the CanvasLayer
+	animation_player.play("show_message")
+	
 	message_timer.start(1)  # Start the timer for 1 second
 
 func _on_message_timer_timeout():
+	# Do nothing on timer timeout
+	pass
+
+func _on_animation_finished(anim_name):
+	if anim_name == "show_message":
+		waiting_for_input = true
+
+func hide_message():
 	message_canvas_layer.visible = false
+	waiting_for_input = false
+
+func _input(event):
+	if waiting_for_input and (event is InputEventMouseButton or event is InputEventKey):
+		hide_message()
